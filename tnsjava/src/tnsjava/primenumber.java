e52bc14d37427a73ba4a2df895835f47331b18e2
srinivas <srinivas@localhost>
2025-09-13 16:48:06 +0530

    primenumber

---
+++ tnsjava/src/tnsjava/primenumber.java
@@ -0,0 +1,16 @@
+package tnsjava;
+public class primenumber {
+	public static void main(String[] args) {
+		System.out.println("primes from 1 to 100 are: ");
+		for (int i = 2; i <= 100; i++) {
+			int count = 0;
+			for (int j = 1; j <= 100; j++) {
+				if (i % j == 0) {
+					count++;
+				}
+			}
+	if (count == 2) {
+		System.out.println(i + " ");}
+	}
+	}
+}
